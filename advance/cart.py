f3549e20776b1f4268ad5d093ed1a9fe1c7c21aa
FreemanFei <pengfei_gao92@hotmail.com>
2017-12-23 15:01:34 +0800

    modify cart_tree

--- advance/cart.py
+++ advance/cart.py
@@ -22,12 +22,12 @@ def regressLeaf(dataset):
     输出：对应数据集的叶节点
     '''
     return np.mean(dataset[:,-1])
-    
 
-def split_dataset(dataset, col, val):
+
+def split_dataset(data_array, col, val):
 	array_1 = data_array[data_array[:,col] >= val,:]
-    array_2 = data_array[data_array[:,col] < val,:]
-    return array_1, array_2
+	array_2 = data_array[data_array[:,col] < val,:]
+	return array_1, array_2
 
 
 def CartTree(data_array):
@@ -41,15 +41,15 @@ def CartTree(data_array):
 		return None,regressLeaf(data_array)
 
 	nrow, ncol = data_array.shape
-	Err = regressErr(dataset)
+	Err = regressErr(data_array)
 
 	bestErr = np.inf; bestCol = 0; bestValue = 0
 
 	for col in xrange( ncol - 1 ):
-		value = list(set( dataset[:,col] ))
+		value = list(set( data_array[:,col] ))
 		for val in value:
-			left, right = split_dataset(dataset, col, val)
-			if (array_1.shape[0] < thresholdSamples) or (array_2.shape[0] < thresholdSamples): continue
+			left, right = split_dataset(data_array , col, val)
+			if (left.shape[0] < thresholdSamples) or (right.shape[0] < thresholdSamples): continue
 
 			tmpErr = regressErr(left) + regressErr(right)
 
@@ -59,17 +59,64 @@ def CartTree(data_array):
 				bestCol = col
 
 	#检验在所选出的最优划分特征及其取值下，误差平方和与未划分时的差是否小于阈值，若是，则不适合划分
-    if (Err - bestErr) < thresholdErr:
-        return None,regressLeaf(dataset)
+	if (Err - bestErr) < thresholdErr:
+		return None,regressLeaf(data_array)
+	return bestCol,bestValue
+
+
+def build_tree(data_array):
+	bestCol,bestValue = CartTree(data_array)
+	if bestCol == None:
+		return node(results = bestValue)
+	else:
+		array_1,array_2 = split_dataset(data_array,bestCol,bestValue)
+		greater_branch = build_tree(array_1)
+		less_branch = build_tree(array_2)
+		return node(col = bestCol,value = bestValue,gb = greater_branch ,lb = less_branch )
+
+
+
+def loadDataSet(fileName):      #general function to parse tab -delimited floats
+    dataMat = []                #assume last column is target value
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = map(float,curLine) #map all elements to float()
+        dataMat.append(fltLine)
+    return dataMat
+
+class node:
+    def __init__(self,col = -1,value = None, results = None, gb = None, lb = None):
+        self.col = col
+        self.value = value
+        self.results = results
+        self.gb = gb
+        self.lb = lb
+
+
+def printtree(tree,indent=''):
+   # Is this a leaf node?
+   if tree.results!=None:
+      print str(tree.results)
+   else:
+      # Print the criteria
+      print str(tree.col)+':'+str(tree.value)
+
+      # Print the branches
+      print indent+'T->',
+      printtree(tree.gb,indent+'  ')
+      print indent+'F->',
+      printtree(tree.lb,indent+'  ')
 
 
 if __name__ == '__main__':
 
-    data = loadDataSet('/Users/fei_Daniel/Desktop/machineLearning-master/CART/ex0.txt')
-    data_array = np.array(data)
+	data = loadDataSet('/Users/fei_Daniel/Desktop/machineLearning-master/CART/ex0.txt')
+	data_array = np.array(data)
 
-    tree = CartTree(data_array)
+	tree = build_tree(data_array)
 
+	print printtree(tree)
 
 
 
